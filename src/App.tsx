0a6a0c9c0a559dbd8adca7a84505608391086175
CodeKrakken <newholm1@hotmail.com>
2026-05-21 16:23:04 +0100

    Create interface component

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,9 @@
 import './App.css';
-import { useEffect, useRef, useState }  from 'react';
-import Voice                            from './components/Voice/Voice';
-import { VoiceType }                    from './components/Voice/Voice.types'
-import Header                           from './components/Header/Header';
-import { setUpVoice }                   from './App.functions';
-import { Synth }                        from './Synth/Synth';
-
+import Interface from './components/Interface/Interface';
 
 function App() {
 
-  // state
-
-  const [voices,  setVoices] = useState<VoiceType[]>([])
-  const [running, setRunning] = useState<boolean>(false)
-
-  // refs
-
-  const runningRef = useRef(false)
-  const voicesRef = useRef(voices)
-
-  // effect
-
-  useEffect(() => { 
-
-    voicesRef.current = voices
-    if (!voices.length) toggleRunning(false)   
-
-  }, [voices])
-
-  // functions
-
-  const handleAddVoice = () => {
-    const newVoice = setUpVoice(voices[voices.length - 1])
-    setVoices(voices => [voices, newVoice].flat())
-    Synth.add(newVoice, running, runningRef, voicesRef)
-  }
-
-  const handleDelete = (i: number) => {
-    const voice = voices[i]
-    voice.isActive = false
-    setVoices(voices => voices.filter((voice, j) => j !== i))
-    Synth.delete(i)
-  }
-
-  const handleStartStop = () => runningRef.current ? stopAll(voices) : start()
-
-  const start = async () => {
-    toggleRunning(true)
-    Synth.start(runningRef, voicesRef)
-  }
-
-  const stopAll = (voices: VoiceType[]) => {
-    toggleRunning(false)
-    Synth.stop()
-  }
-
-  const toggleRunning = (state: boolean) => {
-    runningRef.current = state
-    setRunning(state)
-  }
-
-  return <>
-    <br />
-    <Header 
-      handleStartStop = {handleStartStop}
-      running         = {running}
-      handleAddVoice  = {handleAddVoice}
-      showStart       = {Boolean(voices.length)}
-    />
-
-    <br />
-    <br />
-    {
-      voices.map((voice, i) => 
-        <div key={i}>
-          <Voice
-            i             = {i} 
-            setVoices     = {setVoices} 
-            voices        = {voices}
-            handleDelete  = {handleDelete}
-            dataAttribute = "Voices"
-          />
-        </div>
-      )
-    }
-  </>
+  return <Interface />
 }
 
 export default App;
---
+++ src/components/Interface/Interface.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useRef, useState }  from 'react';
+import Voice                            from '../Voice/Voice';
+import { VoiceType }                    from '../Voice/Voice.types'
+import Header                           from '../Header/Header';
+import { setUpVoice }                   from './Interface.functions';
+import { Synth }                        from '../../Synth/Synth';
+
+
+function Interface() {
+
+  // state
+
+  const [voices,  setVoices] = useState<VoiceType[]>([])
+  const [running, setRunning] = useState<boolean>(false)
+
+  // refs
+
+  const runningRef = useRef(false)
+  const voicesRef = useRef(voices)
+
+  // effect
+
+  useEffect(() => { 
+
+    voicesRef.current = voices
+    if (!voices.length) toggleRunning(false)   
+
+  }, [voices])
+
+  // functions
+
+  const handleAddVoice = () => {
+    const newVoice = setUpVoice(voices[voices.length - 1])
+    setVoices(voices => [voices, newVoice].flat())
+    Synth.add(newVoice, running, runningRef, voicesRef)
+  }
+
+  const handleDelete = (i: number) => {
+    const voice = voices[i]
+    voice.isActive = false
+    setVoices(voices => voices.filter((voice, j) => j !== i))
+    Synth.delete(i)
+  }
+
+  const handleStartStop = () => runningRef.current ? stopAll(voices) : start()
+
+  const start = async () => {
+    toggleRunning(true)
+    Synth.start(runningRef, voicesRef)
+  }
+
+  const stopAll = (voices: VoiceType[]) => {
+    toggleRunning(false)
+    Synth.stop()
+  }
+
+  const toggleRunning = (state: boolean) => {
+    runningRef.current = state
+    setRunning(state)
+  }
+
+  return <>
+    <br />
+    <Header 
+      handleStartStop = {handleStartStop}
+      running         = {running}
+      handleAddVoice  = {handleAddVoice}
+      showStart       = {Boolean(voices.length)}
+    />
+
+    <br />
+    <br />
+    {
+      voices.map((voice, i) => 
+        <div key={i}>
+          <Voice
+            i             = {i} 
+            setVoices     = {setVoices} 
+            voices        = {voices}
+            handleDelete  = {handleDelete}
+            dataAttribute = "Voices"
+          />
+        </div>
+      )
+    }
+  </>
+}
+
+export default Interface;
