48d057486260d730e853c07691da7aab9b5b6662
CodeKrakken <newholm1@hotmail.com>
2026-05-27 11:14:27 +0100

    Refactor Input.test.tsx

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -2,7 +2,9 @@ import { render, screen } from '@testing-library/react';
 import Input from './Input';  
   
 describe('Input', () => {  
+
   it('renders an input element with the given props', () => {  
+    
     render(  
       <Input  
         className="test-class"  
@@ -15,6 +17,7 @@ describe('Input', () => {
     );  
   
     const input = screen.getByDisplayValue('120');  
+
     expect(input).toBeInTheDocument();  
     expect(input).toHaveAttribute('type', 'number');  
     expect(input).toHaveClass('test-class');  
