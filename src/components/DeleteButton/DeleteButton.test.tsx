b8ba2d638a9f7d448341a9cb37c099b092fc4dd5
CodeKrakken <newholm1@hotmail.com>
2026-05-27 10:43:29 +0100

    Refactor DeleteButton test

--- src/components/DeleteButton/DeleteButton.test.tsx
+++ src/components/DeleteButton/DeleteButton.test.tsx
@@ -3,23 +3,22 @@ import DeleteButton from './DeleteButton';
 
 describe('DeleteButton', () => {
 
+  const Button = (i: number) => (
+    <DeleteButton
+      handleDelete={mockHandleDelete}
+      i={i}
+    />
+  );
+
   const mockHandleDelete = jest.fn();
 
   beforeEach(() => { jest.clearAllMocks(); });
 
+  
   it('passes correct index on click', () => {
 
-    const Button = (i: number) => (
-      <DeleteButton
-        handleDelete={mockHandleDelete}
-        i={i}
-      />
-    );
-
     render(Button(1));
-
     const button = screen.getByRole('button');
-
     fireEvent.click(button);
 
     expect(mockHandleDelete).toHaveBeenCalledWith(1);
