98fe52bac461fe9217574aa2aca9a0df7e1f3ee5
CodeKrakken <newholm1@hotmail.com>
2026-06-17 10:19:30 +0100

    Refactor Field input

--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -1,32 +1,32 @@
-import { attributes } from "../../content/data";
-import { InputProps } from "../Input/Input.types";
 import { updateField } from "../Inputs/Inputs.functions";
 import { Atom } from "../shared.types";
 import { VoiceType } from "../Voice/Voice.types";
 
 export default function Field ({
+
   attrName,
   i,
   voices,
   setVoices
+
 } : {
+
   attrName: string,
   i: number,
   voices: VoiceType[],
   setVoices: React.Dispatch<React.SetStateAction<VoiceType[]>>
+
 }) {
 
   const voice = voices[i]
-
-  const attr = attributes[attrName as keyof typeof attributes]
   
-  const props: InputProps = {
+  const props = {
     className: 'textbox',
     'data-voice': i,
-    'data-attribute': `${attr.value}`,
+    'data-attribute': {attrName},
     type: 'number',
-    value: voice[attr.value as Atom],
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => updateField(e, attr.value as Atom, voices, i, setVoices)
+    value: voice[attrName as Atom],
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => updateField(e, attrName as Atom, voices, i, setVoices)
   }
 
   return <div className="row">
