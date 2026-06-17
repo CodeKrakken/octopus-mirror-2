6022bd0ca2d74989ee25faff95a37c9c2f01f07f
CodeKrakken <newholm1@hotmail.com>
2026-06-17 12:54:44 +0100

    Restore InputProps type

--- src/components/Input/Input.types.tsx
+++ src/components/Input/Input.types.tsx
@@ -1,5 +1,18 @@
 import { ChangeEventHandler } from "react"
 
+type InputProps = {
+  className         : string  
+  'data-voice'      : number
+  'data-attribute'  : string
+  type              : string
+  value             : string | number
+  onChange          : ChangeEventHandler
+  checked?          : boolean
+  key?              : string
+  min?              : number
+  max?              : number
+}
+
 type SliderProps = {
   // className        : string  
   // 'data-voice'     : number
@@ -14,5 +27,6 @@ type SliderProps = {
 }
 
 export type { 
+  InputProps,
   SliderProps
 }
