a4bcd8408ab153772183d56f2b21cbacba45b969
CodeKrakken <newholm1@hotmail.com>
2026-06-17 10:36:05 +0100

    Replace attr.value with attrName in DoubleSlider

--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -29,7 +29,9 @@ export default function Field ({
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => updateField(e, attrName as Atom, voices, i, setVoices)
   }
 
-  return <div className="row">
-    <input {...props} />
-  </div>
+  return (
+    <div className="row">
+      <input {...props} />
+    </div>
+  )
 }
