b1a44b28df077a35b47c51a1909764312ee53310
CodeKrakken <newholm1@hotmail.com>
2026-06-24 15:22:27 +0100

    Style voice appearance - no functionality yet

--- src/components/Voice/Voice.tsx
+++ src/components/Voice/Voice.tsx
@@ -15,50 +15,81 @@ export default function Voice(
 ) {
   
   return (
-    <div 
-      className="voice" 
-      data-voice={i}
-      data-attribute={dataAttribute}
-    >
-      <div className="row">
-        <DeleteButton
-          handleDelete={handleDelete}
-          i={i}
-        />
+<div className="voice">
 
-        <TextField 
-          attrName  = {'label'}
-          i         = {i}
-          voices    = {voices}
-          setVoices = {setVoices}
-        />
+  <div className="row">
+    <div className="label block">Label</div>
+    <div className="single block">BPM</div>
+    <div className="single block">Rest</div>
+    <div className="delete block">X</div>
+  </div>
 
-        {
-          Object.keys(singleSliders).map(attrName => (
-            <Input 
-              attrName={attrName}
-              i={i}
-              voices={voices}
-              setVoices={setVoices}
-            />
-          ))
-        }
-      </div>
+  <div className="row">
+    <div className="double block">Length</div>
+    <div className="double block">Offset</div>
+    <div className="double block">Detune</div>
+  </div>
 
-      <div className="row">
-        <div className="column">
-          {
-            Object.keys(doubleSliders).map(attrName => (
-              <Input 
-                attrName={attrName}
-                i={i}
-                voices={voices}
-                setVoices={setVoices}
-              />
-            ))
-          }
-        </div>
-      </div>
-    </div>
+  <div className="row">
+    <div className="double block">Level</div>
+    <div className="double block">Attack</div>
+    <div className="double block">Decay</div>
+  </div>
+
+  <div className="notes block">Notes</div>
+
+  <div className="octaves block">Octaves</div>
+
+  <div className="bottom">
+    <div className="sounds block">Sounds</div>
+    <div className="intervals block">Intervals</div>
+  </div>
+
+</div>
+    // <div 
+    //   className="voice" 
+    //   data-voice={i}
+    //   data-attribute={dataAttribute}
+    // >
+    //   <div className="row">
+    //     <DeleteButton
+    //       handleDelete={handleDelete}
+    //       i={i}
+    //     />
+
+    //     <TextField 
+    //       attrName  = {'label'}
+    //       i         = {i}
+    //       voices    = {voices}
+    //       setVoices = {setVoices}
+    //     />
+
+    //     {
+    //       Object.keys(singleSliders).map(attrName => (
+    //         <Input 
+    //           attrName={attrName}
+    //           i={i}
+    //           voices={voices}
+    //           setVoices={setVoices}
+    //         />
+    //       ))
+    //     }
+    //   </div>
+
+    //   <div className="row">
+    //     <div className="column">
+    //       {
+    //         Object.keys(doubleSliders).map(attrName => (
+    //           <Input 
+    //             attrName={attrName}
+    //             i={i}
+    //             voices={voices}
+    //             setVoices={setVoices}
+    //           />
+    //         ))
+    //       }
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
