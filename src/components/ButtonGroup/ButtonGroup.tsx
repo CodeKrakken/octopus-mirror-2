8842c71f3c82477ecf60ee693ec525acd1c78f92
CodeKrakken <newholm1@hotmail.com>
2026-07-01 12:33:52 +0100

    Hide and show button groups - not styled yet

--- src/components/ButtonGroup/ButtonGroup.tsx
+++ src/components/ButtonGroup/ButtonGroup.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ButtonProps } from "../Input/Input.types";
 import { updateButton } from "../Inputs/Inputs.functions";
 import { ButtonGroupType } from "../shared.types";
@@ -22,48 +23,76 @@ export default function ButtonGroup({
   setVoices
 }: ButtonGroupProps) {
 
+  const [hidden, setHidden] = useState(true)
+
   const voice = voices[i]
 
+  let imgSrc
+
+  try {
+    imgSrc = require(`./images/${group.className}/${group.className}.png`) || ""
+  } catch (error) {
+    console.error(error instanceof Error ? error.message : "Unknown error", error)
+  }
+
+  const handleClick = () => {
+    setHidden((prev) => !prev)
+  }
+
   return <>
+
+    <button 
+      style={{height: "56px", width: "56px"}}
+      onClick={handleClick}
+    >
+      {
+        imgSrc ? <img src={imgSrc} width="100%" height="100%" />
+        : <>{group.label}</>
+      }
+    </button>
+
     {
-      group.boxes.map(button => {
+      hidden ? <></>
+        :
+      <>
+        {
+          group.boxes.map(button => {
 
-        const props = {
-          className: `
-            key
-            ${voice[`active${group.label as ButtonGroupType}`].includes(button) ? 'active' : ''}
-          `,
-          'data-attribute': group.label,
-          'data-voice': i,
-          value: button,
-          checked: voice[`active${group.label as ButtonGroupType}`].includes(button),
-          onClick: (e: React.MouseEvent<HTMLButtonElement>) => updateButton(e, `active${group.label as ButtonGroupType}`, voices, i, setVoices),
-          key: button
-        };
+            const props = {
+              className: `
+                key
+                ${voice[`active${group.label as ButtonGroupType}`].includes(button) ? 'active' : ''}
+              `,
+              'data-attribute': group.label,
+              'data-voice': i,
+              value: button,
+              checked: voice[`active${group.label as ButtonGroupType}`].includes(button),
+              onClick: (e: React.MouseEvent<HTMLButtonElement>) => updateButton(e, `active${group.label as ButtonGroupType}`, voices, i, setVoices),
+              key: button
+            };
 
-        console.log(group)
+            console.log(group)
 
-        let imgSrc
+            let imgSrc
 
-        try {
-          imgSrc = require(`./images/${group.className}/${button}.png`) || ""
+            try {
+              imgSrc = require(`./images/${group.className}/${button}.png`) || ""
 
-        } catch (error) {
-          console.error(error instanceof Error ? error.message : "Unknown error", error)
-        }
-
-        console.log('imgSrc')
-        console.log(imgSrc)
-
-        return (
-          <button {...props} style={{height: "28px", width: "28px"}}>
-            {
-              imgSrc ? <img src={imgSrc} width="100%" height="100%" />
-              : <>{button}</>
+            } catch (error) {
+              console.error(error instanceof Error ? error.message : "Unknown error", error)
             }
-          </button>
-        )       
-      })
+
+            return (
+              <button {...props} style={{height: "28px", width: "28px"}}>
+                {
+                  imgSrc ? <img src={imgSrc} width="100%" height="100%" />
+                  : <>{button}</>
+                }
+              </button>
+            )       
+          })
+        }
+      </>
     }
   </>
 }
