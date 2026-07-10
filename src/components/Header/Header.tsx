2c77778799edc355b5d8897e644cdd78239e7817
CodeKrakken <newholm1@hotmail.com>
2026-07-10 18:03:52 +0100

    Resolve final key based errors

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -34,8 +34,8 @@ export default function Header ({
 
   const font = (string: string, height: string = "40px") => {
 
-    const imageArray = string.split('').map(letter => 
-      /^[A-Z0-9]*$/.test(letter.toUpperCase()) ? <img alt="" src={require(`../../content/font-images/${letter.toUpperCase()}.png`)} height={height} key={letter} />  : letter
+    const imageArray = string.split('').map((letter, i) => 
+      /^[A-Z0-9]*$/.test(letter.toUpperCase()) ? <img alt="" src={require(`../../content/font-images/${letter.toUpperCase()}.png`)} height={height} key={`${letter}-${i}`} />  : letter
     )
 
     return <div className="centred">{imageArray.map(image => image)}</div>
